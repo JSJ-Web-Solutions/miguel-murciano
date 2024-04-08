753ebad2b05be511cc47068375cdc894bf7c1dfc
sleklere <santiagoleklere@gmail.com>
2024-04-08 20:57:15 -0300

    about me and contact sections layout

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <div className="about" id="about">
+      <h1>about me</h1>
+      <div className="row">
+        <p></p>
+      </div>
+      <div className="row">
+        <p></p>
+      </div>
+      <div className="row">
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,36 @@
+function Contact() {
+  return (
+    <section className="contact" id="contact">
+      <div>
+        <ul>
+          <li>
+            <p>WORLDWIDE DIRECTOR</p>
+          </li>
+          <li>
+            <p>MEXICO, COLOMBIA AND CHILE THE MAESTROS</p>
+          </li>
+          <li>
+            <a href="www.themaestros.tw" target="_blank">
+              WWW.THEMAESTROS.TW
+            </a>
+          </li>
+          <li>
+            <p>
+              REPRESENTED IN ASIA / EASTER EUROPE / MIDDLE EAST / NORTH AFRICA
+              BY GO EAST
+            </p>
+          </li>
+          <li>
+            <p>@MIGUEL_MURCIANO</p>
+          </li>
+          <li>
+            <p>INFO@MIGUELMURCIANO.COM</p>
+          </li>
+        </ul>
+      </div>
+      <h1>contact</h1>
+    </section>
+  );
+}
+
+export default Contact;
